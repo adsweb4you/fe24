86c968825b53b6895cc3780c0312d1706f575634
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2021-10-06 21:44:27 +0400

    js

---
+++ js/mine.js
@@ -0,0 +1,23 @@
+
+let string = "sdasd5r43czx345"; //სტრინგი
+let number  = 4;
+let boolean = true;
+let boolfalse  = false;
+let object = {color:"red", height:"120px"};
+let arraY = ["SAD", "ASDAS" , "ASD"]; 
+let undefineds = undefined;
+let nulls = null;
+let nn = NaN;
+let fun  = function(){};
+ 
+ function calc(z){
+    document.getElementById("mydiv").innerHTML = "თქვენი მასის ინდექსი არის" +   z * 4 ; 
+ }
+
+ calc(5, 8, 7);
+
+ let myname  = `მე ვარ   პროგრამისტი   ${number}   წლიანი გამოცდილებით` ;
+
+
+ document.getElementById("mydiv").innerHTML = myname
+
